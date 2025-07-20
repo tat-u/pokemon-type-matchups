3174956d11c99822c25eefb31df10df568731909
t_ueda <uetatsu1005@gmail.com>
2025-07-21 00:10:41 +0900

    Delete unrelated files

--- src/stories/atoms/FieldError.tsx
+++
@@ -1,7 +0,0 @@
-import { ComponentProps } from "react";
-
-type Props = Omit<ComponentProps<"div">, "className">;
-
-export function FieldError(props: Props) {
-  return <div {...props} className="h-[24px] text-sm/[24px] text-red-600" />;
-}
