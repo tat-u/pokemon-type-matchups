a827310909bec0b85bd53ad8940ada040c3fc804
tat-u <uetatsu1005@gmail.com>
2025-11-15 20:30:16 +0900

    Update styles

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -16,30 +16,36 @@ export default function Page() {
     <>
       <Header className="w-full sticky top-0" />
       <main className="flex flex-col gap-5 items-center p-5">
-        <div className="w-60 text-sm text-center">
+        <p className="w-full text-sm text-center">
           あなたのポケモンのタイプとわざのタイプから、各タイプの相手ポケモンとの相性を調べます。
-        </div>
-        <div className="w-70 text-sm flex flex-col gap-4">
-          <div className="flex gap-4 items-center">
+        </p>
+
+        <div className="w-70 flex flex-col gap-3">
+          <div className="flex gap-3 items-center">
             <Sword className="size-5" />
-            このタイプのポケモンを攻撃したときの有利・不利
+            <span className="text-sm w-full">
+              このタイプのポケモンを攻撃したときの有利・不利
+            </span>
           </div>
-          <div className="flex gap-4 items-center">
+          <div className="flex gap-3 items-center">
             <Shield className="size-5" />
-            このタイプのわざを受けたときの有利・不利
+            <span className="text-sm w-full">
+              このタイプのわざを受けたときの有利・不利
+            </span>
           </div>
         </div>
-        <div className="flex flex-col gap-2 text-sm w-60">
+
+        <div className="flex flex-col gap-3 w-60">
           <div className="flex justify-between items-center">
-            <div>タイプ 1</div>
+            <span className="text-sm">タイプ 1</span>
             <Select options={options} defaultValue="normal" name="type1" />
           </div>
           <div className="flex justify-between items-center">
-            <div>タイプ 2</div>
+            <span className="text-sm">タイプ 2</span>
             <Select options={options} defaultValue="normal" name="type2" />
           </div>
           <div className="flex justify-between items-center">
-            <div>わざのタイプ</div>
+            <span className="text-sm">わざのタイプ</span>
             <Select options={options} defaultValue="normal" name="moveType" />
           </div>
         </div>
